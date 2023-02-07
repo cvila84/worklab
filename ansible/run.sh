b77760cab8443c579198bb4ebe4469c849ef6b2e
Christophe VILA <christophe.vila@thalesgroup.com>
2023-02-07 18:57:00 +0100

    Update

--- ansible/run.sh
+++ ansible/run.sh
@@ -2,4 +2,4 @@
 export ANSIBLE_HOST_KEY_CHECKING="False"
 export ANSIBLE_SSH_ARGS="-F /home/kriss/.ssh/config"
 #ansible -i inventory.yaml -m ping all
-ansible-playbook -i inventory.yaml -e @vars.yaml playbooks/docker.yaml
+ansible-playbook -i inventory.yaml -e @vars.yaml playbooks/rke.yaml
